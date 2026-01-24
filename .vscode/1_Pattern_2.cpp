0987021948adfda2ce29366a83b31869d8f83db6
945mv <mahakverma236@gmail.com>
2026-01-24 14:20:53 +0530

    Pattern practice 2

--- .vscode/1_Pattern_2.cpp
+++ .vscode/1_Pattern_2.cpp
@@ -2,21 +2,13 @@
 using namespace std;
 int main() {
     int n;
+    cout<<"Enter the value of n: ";
     cin>>n;
-    for (int i=1;i<=n-1;i++) {
-        for (int j=1;j<=n-i;j++) {
-            cout<<" ";
-        } for(int j=1;j<=i;j++) {
-            cout<<"* ";
-        }
-        cout<<endl;
-    } for (int i=1;i<=n-1;i++) {
-        for (int j=1;j<=n-i;j++) {
-            cout<<" ";
-        } for(int j=1;j<=i;j++) {
+    for (int i=n;i>=1;i--) {
+        for (int j=i;j>0;j--) {
             cout<<"* ";
         }
         cout<<endl;
     }
-return 0;
+    return 0;
 }
