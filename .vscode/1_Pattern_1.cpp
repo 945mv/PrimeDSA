bc47d97c7089f3f823696852ca6ffc1ec11bd151
945mv <mahakverma236@gmail.com>
2026-01-24 13:55:37 +0530

    Pattern practice 1

--- .vscode/1_Pattern_1.cpp
+++ .vscode/1_Pattern_1.cpp
@@ -1,33 +1,15 @@
 #include <iostream>
-
 using namespace std;
-
 int main() {
     int n;
-    cout << "Enter n: ";
-    cin >> n;
-
-    // Outer loop for rows
-    for (int i = 1; i <= n; i++) {
-        
-        // Print leading spaces for alignment
-        for (int j = 1; j <= (n - i); j++) {
-            cout << "  "; 
+    cout<<"Enter the value of n: ";
+    cin>>n;
+    for (int i=1;i<=n;i++) {
+        for (int j=1;j<=i;j++) {
+            cout<<"* ";
         }
-
-        // Print characters for the current row
-        for (int k = 1; k <= (2 * i - 1); k++) {
-            // Logic: Even positions in the current row segment get '!' 
-            // BUT only if they are not on the top row.
-            if (i > 1 && k % 2 == 0) {
-                cout << "! ";
-            } else {
-                cout << "* ";
-            }
-        }
-
-        cout << endl;
+        cout<<endl;
     }
-
     return 0;
+    
 }
