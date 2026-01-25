c23926e307b77528ab0ef58b1d1d10e64e4cd8c2
945mv <mahakverma236@gmail.com>
2026-01-25 21:31:54 +0530

    Pattern Practice 4

--- .vscode/1_Pattern_4.cpp
+++ .vscode/1_Pattern_4.cpp
@@ -1,22 +1,15 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 int main() {
     int n;
-    cout<<"Enter the value of n: ";
     cin>>n;
-for (int i=1;i<=n;i++) 
-{
-    for (int j=1;j<=i;j++)
-     {
-    cout<<"* ";
-       }
-    cout<<endl;
+    for (int i=n;i>=1;i--) {
+        for (int k=n-i;k>0;k--) {
+            cout<<" ";
+        } for (int j=i;j>0;j--) {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
 }
-for (int i=n-1;i>=1;i--) {
-    for (int j=1;j<=i;j++) {
-        cout<<"* ";
-    } 
-    cout<<endl;
-}
-return 0;
-} 
