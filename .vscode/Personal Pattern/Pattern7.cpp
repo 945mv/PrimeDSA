ee7559d937ef00adc7153d5d4e861976aa4ef232
945mv <mahakverma236@gmail.com>
2026-01-30 20:31:58 +0530

    Row Asymmetric 1

--- .vscode/Personal Pattern/Pattern7.cpp
+++ .vscode/Personal Pattern/Pattern7.cpp
@@ -2,13 +2,11 @@
 using namespace std;
 int main() {
     int n;
+    cout<<"Enter the value of n: ";
     cin>>n;
     for (int i=1;i<=n;i++) {
-        for (int k=n-i;k>0;k--) {
-            cout<<" ";
-        }
-        for (int j=1;j<=i;j++) {
-            cout<<i;
+        for (int j=1;j<=n-i+1;j++) {
+            cout<<"*";
         }
         cout<<endl;
     }
