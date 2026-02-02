aba467e4dbe5d0497e647367f10d88be801965c6
945mv <mahakverma236@gmail.com>
2026-02-02 16:17:03 +0530

    gcd stl

---
+++ C++ Programming/gcd_stl.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+//__gcd() 
+#include <algorithm>
+// -infinity and +infinity
+#include <climits>
+using namespace std;
+int main() {
+    int c=INT_MAX; //+infinity
+    int d=INT_MIN; //-infinity
+    int a=5;
+    int b=10;
+    cout<<__gcd(a,b);
+    return 0;
+}
