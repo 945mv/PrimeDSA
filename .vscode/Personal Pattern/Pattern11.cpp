0b1649b0d8541431f360329e185ed539b56c4bf8
945mv <mahakverma236@gmail.com>
2026-02-08 00:46:21 +0530

    Pattern Question

---
+++ .vscode/Personal Pattern/Pattern11.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+    for (int i=n;i>=1;i--) {
+        for (int j=1;j<=2*i-2;j++) {
+            cout<<" ";
+        }
+        for (int j=1;j<=i;j++) {
+            cout<<"* ";
+        }
+        cout<<endl<<" ";
+    }
+    for (int i=2;i<=n;i++) {
+        for (int j=1;j<=2*i-2;j++) {
+            cout<<" ";
+        }
+        for (int j=1;j<=i;j++) {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
