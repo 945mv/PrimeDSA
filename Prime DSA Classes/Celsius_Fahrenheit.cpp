34d9ff38d957150c46087ee40c48f6ee67f9f576
945mv <mahakverma236@gmail.com>
2026-02-20 22:12:18 +0530

    Celsius Fahrenheit

---
+++ Prime DSA Classes/Celsius_Fahrenheit.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"ENTER THE VALUES: ";
+    int min,max,step;
+    cin>>min>>max>>step;
+    for(int i=min;i<=max;i+=step){
+        int c=(5.0/9.0)*(i-32);
+        cout<<i<<" "<<c<<endl;
+    }
+}
