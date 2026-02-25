f23d661f0b42d5cd20dfa466f00a336b9952955a
945mv <mahakverma236@gmail.com>
2026-02-25 22:41:47 +0530

    Leetcode Q3658

---
+++ Prime DSA Classes/LeetCode/Q3658.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int gcdOfOddEvenSums(int n) {
+    return n;
+}
+int main() {
+    cout<<"Enter the value of n: ";
+    int n;
+    cin>>n;
+    cout<<gcdOfOddEvenSums(n)<<endl;
+    return 0;
+}
