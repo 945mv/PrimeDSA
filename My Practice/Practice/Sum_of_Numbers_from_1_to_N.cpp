11b155a9c3029ec7cbf9cc4ee6c0637f55880ec5
945mv <mahakverma236@gmail.com>
2026-03-05 21:26:08 +0530

    Sum of Number from 1 to N

---
+++ My Practice/Practice/Sum_of_Numbers_from_1_to_N.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+    int sum=n*(n+1)/2;
+    cout<<"Sum of Numbers from 1 to "<<n<<" is:"<<sum;
+    }
