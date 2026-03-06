de1ffbd36a36c8fd08686362abd4858e078a7c72
945mv <mahakverma236@gmail.com>
2026-03-07 00:54:29 +0530

    Typecasting and Input

---
+++ My Practice/Practice/TypeCasting_and_Input.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {  //TypeCasting
+ //Conversion (Implicit) -->Small to big
+ char Name='M'; //M kee ASCII value "77" hai
+ int age=Name;
+ cout<<age<<endl;
+ //Casting (Explicit) --> Big to small
+ double salary=9000.89; //9000.89 mai . ke baad kuch bhi kyu naa likha woh point ke pahle kee value hee deta hai 
+ int newSalary=(int)salary;
+ cout<<newSalary<<endl;
+ //Input kaise lete hai
+ double weight;
+ cout<<"Enter your weight : ";
+ cin>>weight;
+ cout<<"Your weight is: "<<weight<<endl;
+    return 0;
+}
