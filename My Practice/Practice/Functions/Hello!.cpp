1386dc90fb2e7ffb7a2fe6867e226c7a2e3b7cf9
945mv <mahakverma236@gmail.com>
2026-03-12 19:47:38 +0530

    Functions

---
+++ My Practice/Practice/Functions/Hello!.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+//function definition
+int printHello() {
+    cout<<"Hello Mahak!"<<endl;
+    return 9;
+}
+
+int main() {
+//function call / invoke
+
+   cout<<printHello()<<endl;
+   // int val=printHello();
+   // cout<<"Mahak's Birth Date: "<<val<<endl;
+    return 0;
+}
