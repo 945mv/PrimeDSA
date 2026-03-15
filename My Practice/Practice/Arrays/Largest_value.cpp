ecb14327b39aa7aaf04c0b7368289f8d6525a77e
945mv <mahakverma236@gmail.com>
2026-03-15 23:16:55 +0530

    Array practice

---
+++ My Practice/Practice/Arrays/Largest_value.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<climits> //for mathematical expression
+using namespace std;
+int main() {
+    int n;
+    cout<<"Enter the size of Array: ";
+    cin>>n;
+    int arr[n];
+    for (int i=0;i<n;i++) {
+        cin>>arr[i];
+    }
+    int largest= INT_MIN ; //-Infinity (-infinity isliye likha kee kyoki yeah max choti value deta hai toh iske andar kee sari value max hee hogi )
+    for (int i=0;i<n;i++) {
+    largest=max(arr[i],largest);
+    }
+    cout<<"Largest value is "<<largest;
+    return 0;
+}
