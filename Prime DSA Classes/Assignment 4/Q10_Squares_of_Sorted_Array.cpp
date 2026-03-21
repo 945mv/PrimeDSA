a601b014c1092bbef1b39ce08d4e887c04e9482d
945mv <mahakverma236@gmail.com>
2026-03-22 00:53:45 +0530

    Practice

--- Prime DSA Classes/Assignment 4/Q10_Squares_of_Sorted_Array.cpp
+++ Prime DSA Classes/Assignment 4/Q10_Squares_of_Sorted_Array.cpp
@@ -3,8 +3,10 @@
 using namespace std;
 int main() {
     int n;
+    cout<<"Enter the size of an Array: ";
     cin>>n;
     int arr[n];
+    cout<<"Enter the number of elements in an Array: ";
     //input lena
     for (int i=0;i<n;i++) {
         cin>>arr[i];
@@ -17,6 +19,7 @@ int main() {
     sort(arr,arr+n);
 
     //print karane ke liye
+    cout<<"Squares of Sorted Array is  ";
     for (int i=0;i<n;i++) {
         cout<<arr[i]<<" ";
     }
