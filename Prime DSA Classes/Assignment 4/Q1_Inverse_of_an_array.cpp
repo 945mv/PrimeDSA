ec9d0ae9e0efcb25a4847a09aabd463c452b01fc
945mv <mahakverma236@gmail.com>
2026-03-21 15:26:04 +0530

    Practice

--- Prime DSA Classes/Assignment 4/Q1_Inverse_of_an_array.cpp
+++ Prime DSA Classes/Assignment 4/Q1_Inverse_of_an_array.cpp
@@ -1,18 +1,22 @@
 #include<iostream>
 using namespace std;
-//Inverse of an Array
+//Inverse of an array :- jisme index-->value & value-->index
 int main() {
-	int n;
-	cin>>n;
-	int arr[n],val[n];
-	for(int i=0;i<n;i++){
-		cin>>arr[i];
-	}
-	for(int i=0;i<n;i++){
-		val[arr[i]]=i;
-	}
-	for(int i=0;i<n;i++){
-		cout<<val[i]<<" ";
-	}
-	return 0;
+    int n;
+    cin >> n;
+    int arr[n], val[n];
+    // input array
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    // inverse logic
+    for (int i = 0; i < n; i++) {
+        val[arr[i]] = i;   // val-->index 
+    }
+    // print result
+    for (int i = 0; i < n; i++) {
+        cout << val[i] << " ";   //value print 
+    }
+
+    return 0;
 }
