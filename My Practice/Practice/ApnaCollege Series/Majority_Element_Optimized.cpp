23220fd1849f278895ce62f43aa8e7c9d7cfca98
945mv <mahakverma236@gmail.com>
2026-04-04 17:55:35 +0530

    Practice

--- My Practice/Practice/ApnaCollege Series/Majority_Element_Optimized.cpp
+++ My Practice/Practice/ApnaCollege Series/Majority_Element_Optimized.cpp
@@ -1,9 +1,44 @@
 #include<iostream>
+#include<algorithm>
 #include<vector>
 using namespace std;
 //Time Complexity --> O(nlogn)
 vector<int> MajorityElement(vector<int> nums) {
     vector<int>ans;
     int n=nums.size();
-    
+
+    sort(nums.begin(),nums.end());
+
+    int freq=1;
+    for (int i=1;i<n;i++) {
+        if(nums[i]==nums[i-1]) {
+            freq++;
+        }
+        else {
+            freq=1;
+        }
+        if(freq>n/2) {
+            ans.push_back(nums[i]);
+            return ans;
+        }
+    }
+    return ans;
+}
+int main() {
+    int n;
+    cout<<"Enter the size of an Array: ";
+    cin>>n;
+
+    vector<int> nums(n);
+    for (int i=0;i<n;i++) {
+        cin>>nums[i];
+    }
+    vector<int> ans=MajorityElement(nums);
+    if(ans.size()>0) {
+        cout<<ans[0];
+    }
+    else {
+        cout<<"No Majority Element!";
+    }
+    return 0;
 }
