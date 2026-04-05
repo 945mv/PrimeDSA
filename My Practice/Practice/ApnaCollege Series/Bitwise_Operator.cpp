c4e994b87cb4082cce066bf6c7949a83f45c8c22
945mv <mahakverma236@gmail.com>
2026-04-05 22:15:23 +0530

    Practice

---
+++ My Practice/Practice/ApnaCollege Series/Bitwise_Operator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+    cout<<"Bitwise AND Operator --> " << (6&10) <<endl;
+    cout<<endl;
+    cout<<"Bitwise OR Operator --> " << (6|10) <<endl;
+    cout<<endl;
+    cout<<"Bitwise XOR Operator --> " << (6^10) <<endl;
+    cout<<endl;
+    cout<<"Bitwise Left Shift Operator --> " << (6<<10) <<endl;
+    cout<<endl;
+    cout<<"Bitwise Right Shift Operator --> " << (6>>10) <<endl;
+    return 0;
+}
