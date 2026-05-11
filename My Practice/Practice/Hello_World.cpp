48c2a4dde2102a0c30555c126ba8146481427c01
945mv <mahakverma236@gmail.com>
2026-05-11 21:31:19 +0530

    Practice

---
+++ My Practice/Practice/Hello_World.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+    cout<<"Hello World!!"<<endl;
+    cout<<"My name is Mahak Verma."<<endl;
+    cout<<"I am currently learninng Programming languages."<<endl;
+    cout<<"I want to be Master in C Programming language."<<endl;
+    cout<<"I want to build the big projects by my own."<<endl;
+    return 0;
+}
