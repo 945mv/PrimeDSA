4b3de8ed39c984bd074e57cb488bc89e80d8760a
945mv <mahakverma236@gmail.com>
2026-05-12 14:21:33 +0530

    Delete

--- My Practice/Practice/Hello_World!.cpp
+++
@@ -1,9 +0,0 @@
-//Boilerplate Code
-#include <iostream>
-using namespace std;
-int main() {
-    cout<<"Hello World!!"<<endl;   //"\n"
-    cout<<"I am a Learner!"<<endl;  //"\n"
-    cout<<"I am starting my Journey!"<<endl;  //"\n"=For next line
-    return 0;
-}
