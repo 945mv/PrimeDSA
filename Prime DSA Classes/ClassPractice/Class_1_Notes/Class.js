dfe9db0d375a34b52f495596ab451471c522280e
945mv <mahakverma236@gmail.com>
2026-06-29 12:29:32 +0530

    Practice

---
+++ Prime DSA Classes/ClassPractice/Class_1_Notes/Class.js
@@ -0,0 +1,28 @@
+let fruits=["Apple","Mango","Banana","Lichi","Strawberry"];
+//length=Array ka size batana
+console.log(fruits.length);
+//push=add elements in the last
+//pop=element remove kar dega last se
+//unshift=element ko starting mai daal dega
+//shift=shuru ke index mai jo value hai usko remove kar dega
+//includes=check karta hai ke element present hai kee nhi
+//indexof=uss element ka index ko return kar dega
+//join=saare elements join kar degi jisse bhi join karana ho
+//slice=elements batayega uss range ke right bound se just pahle
+//splice=elemnts batayega uss range ke right bound tak
+//normal for loop
+//for of loop
+//for each
+//ADVANCED ARRAY METHOD:-
+//map = saare elements par conditions apply kar dega
+//filter = tumhari condition karega - jo bhi element condition pass hoga voh new array me aa jaayega
+//reduce = saare elements ko ek variable me convert kar dega
+//find = jaise hee condition satisfied hui vaise he return kar do
+//some() = batata hai condition elements satisfy kar rahe hai yaa nhi 
+//every() = batata hai kee saare elements condition satisfy kar raha hai yaa nhi
+//endswith = batata hai kee koi word usse end ho raha hai kee nhi
+//startswith = batata hai kee aisa koi word joh string mai start ho raha hai
+//indexof = index batayega kee konsa index par word hai 
+//lastindexof = piche se uska index dekhne lagega
+//slice = range me jitna elements padhe hai woh return kar
+//substring = 
