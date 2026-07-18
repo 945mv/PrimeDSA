7b87d0447a7399633be75dd3c195d9616dabf0cf
945mv <mahakverma236@gmail.com>
2026-07-18 19:41:07 +0530

    practice

--- My Practice/Practice/Loops/Patterns/Pyramid_Alphabet_Pattern.cpp
+++ My Practice/Practice/Loops/Patterns/Pyramid_Alphabet_Pattern.cpp
@@ -8,7 +8,7 @@ int main() {
     for (int i=0;i<n;i++) {
         //spaces n-i-1
         for (int j=0;j<n-i-1;j++) {
-            cout<<" ";
+            cout<<" ";v
         }
         //nums1 i+1
         for (int j=1;j<=i+1;j++) {
